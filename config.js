b78dd9b9c0ab54647130dbcd840e61fa0734393f
Reika <56439343+Nich87@users.noreply.github.com>
2021-10-16 12:22:40 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    channel: "Text Channel ID",
+    messageTemplate: "新しい動画が公開されました\n **{author}** が **{title}** \nをアップロードしました!\n{url}",
+    channel_id: ["ch1","ch2","ch3"],
+    watchInterval: 30000
+}
